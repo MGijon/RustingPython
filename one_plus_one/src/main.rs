632633903c7e29cb4848885dd93f375fc9c6b2bb
Manuel Gijón Agudo <mgijon94@gmail.com>
2022-05-25 18:53:47 +0200

    convergence improved in a silly way

--- one_plus_one/src/main.rs
+++ one_plus_one/src/main.rs
@@ -23,9 +23,14 @@ fn sphere(x: f64, y: f64) -> f64{
 fn update_value(original_value: f64) -> f64{
     // add a perturbation to the given value
     let mut rng = rand::thread_rng();
-    // TODO: A lot of work to do here, it is not converging fast enough!
     let perturbation = rng.gen::<f64>();
-    let b: f64 = original_value - perturbation;
+    let coin = rng.gen::<f64>();
+    let b: f64;
+    if coin >= 0.5{
+        b = original_value - perturbation;
+    } else {
+        b = original_value + perturbation;
+    }
     b
 }
 
@@ -36,26 +41,21 @@ fn main() {
 
     let mut initial_value = sphere(x, y);
     println!("Initial spehre value: {}", initial_value);
-    let number_steps: usize = 100;
+    let number_steps: usize = 10000;
 
-    //let mut rng = rand::thread_rng();
     for i in 0..number_steps{
-        //let mut new_x = &x + rng.gen::<f64>();  // plus a random float
-        //let mut new_y = &y + rng.gen::<f64>();
         let new_x = update_value(x);
         let new_y = update_value(y);
 
         let new_sphere_value: f64 = sphere(new_x, new_y);
         if new_sphere_value < initial_value{
-            //println!("Entra!");
-            //println!("{}", new_sphere_value);
             // update sphere benchmark
             initial_value = new_sphere_value;
             x = new_x;
             y = new_y;
         }
         if i % 1000 == 0{
-            println!("Iteration: {}", i);
+            println!("Iteration: {} - Value: {}", i, initial_value);
         }
     }
     println!("{} {}: {}", x, y, initial_value);
